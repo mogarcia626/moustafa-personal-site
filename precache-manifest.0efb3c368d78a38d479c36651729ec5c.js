08e605a3a5b419b4bdfd554bc8b37bb45514ce37
moustafa <mogarcia626@gmail.com>
2021-09-21 11:06:12 -0400

    Updates

---
+++ precache-manifest.0efb3c368d78a38d479c36651729ec5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c569f81945830758cbdcf0035f23448",
+    "url": "/index.html"
+  },
+  {
+    "revision": "565eff51f29be8a29a1f",
+    "url": "/static/css/main.e79be717.chunk.css"
+  },
+  {
+    "revision": "fd3cd865bf0b8ec90650",
+    "url": "/static/js/2.c2c11b4b.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.c2c11b4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "565eff51f29be8a29a1f",
+    "url": "/static/js/main.f4bb29e2.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "6392d98721de62b144fea8bd84c711eb",
+    "url": "/static/media/moustafagarcia.6392d987.jpg"
+  }
+]);
